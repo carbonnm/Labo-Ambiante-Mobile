679b97ac5d5cb8e831cc0bf3e728ade26e209722
carbonnm <marie.carbonnelle@student.unamur.be>
2023-11-02 14:06:57 +0100

    Code du moteur servo

--- Code pour le Raspberry Pi/servoCode.py
+++ Code pour le Raspberry Pi/servoCode.py
@@ -1,10 +1,32 @@
 import RPi.GPIO as GPIO
 import time
 
+#from Phidget22.Phidget import *
+#from Phidget22.Devices.RCServo import *
+
 #Habituellement, on attache le servo à la pin 11 du raspberry, qui sera donc un pin d'output
 
 GPIO.setmode(GPIO.board)
 GPIO.setup(11, GPIO.OUT)
 
-#Création d'un object PWM de contrôle avec une fréquence de 50 hertz
-pwm = GPIO.PWM(11, 50)
+#Création du RCServo avec une fréquence de 50 Hz
+servo = GPIO.PWM(11, 50)
+
+#Start the servo but with value of 0 (no pulse)
+servo.start(0)
+time.sleep(1)
+
+#Move the servo
+
+repetitions = 5
+duty = 2
+while repetitions > 0:
+    while duty <= 12:
+        servo1.ChangeDutyCycle(duty)
+        duty += 1
+    repetitions -= 1
+    time.sleep(1)
+
+#Fin 
+servo.stop()
+GPIO.cleanp()
