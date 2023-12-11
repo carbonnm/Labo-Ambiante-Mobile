42198fdaa170038ff81a45273e8c8f28b81bead2
carbonnm <marie.carbonnelle@student.unamur.be>
2023-12-11 11:26:50 +0100

    Architecture des différents acteurs

---
+++ SmartRPGScala/smartrpg/src/main/scala/IoTSupervisor/DeviceManager/GameGroup.scala
@@ -0,0 +1,10 @@
+package main.scala
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.PostStop
+import akka.actor.typed.Signal
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
