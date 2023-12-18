8d0faae86d4a7d063473d5521d927c533e2a2260
Simon POLET <32013966+Fogbose@users.noreply.github.com>
2023-12-18 13:44:32 +0100

    Remise au point de Pi4J

--- smartrpg/src/main/scala/example/Hello.scala
+++ smartrpg/src/main/scala/example/Hello.scala
@@ -1,3 +1,38 @@
-import psksvp.RPi.SenseHAT._
+package example
 
-val sensors = sensors
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinDirection;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
+import com.pi4j.io.gpio.trigger.GpioPulseStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.event.GpioPinListener;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.event.PinEventType;
+
+object Hello extends Greeting with App {
+  // create gpio controller instance
+  val gpio: GpioController = GpioFactory.getInstance
+  
+  val myLed = gpio.provisionDigitalOutputPin(
+    RaspiPin.GPIO_04,         
+    "MyLed",                    
+    PinState.LOW
+  )
+
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
