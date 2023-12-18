8c8b16fda6b7c19c2316ec6d98393d026b73dd70
Simon POLET <32013966+Fogbose@users.noreply.github.com>
2023-12-18 11:15:41 +0100

    Retour à un exemple simple

--- smartrpg/src/main/scala/example/Hello.scala
+++ smartrpg/src/main/scala/example/Hello.scala
@@ -1,3 +1,5 @@
+package example
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPin;
@@ -17,39 +19,20 @@ import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.io.gpio.event.PinEventType;
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.concurrent.duration._
 
-object BlinkLedScala extends App {
-
+object Hello extends Greeting with App {
   // create gpio controller instance
-  val gpio = GpioFactory.getInstance()
+  val gpio: GpioController = GpioFactory.getInstance
 
-  // provision gpio pin #04 as an output pin and make sure it is set to LOW at startup
-  val myLed: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "My LED", PinState.LOW)
+  val myLed = gpio.provisionDigitalOutputPin(
+    RaspiPin.GPIO_04,         
+    "MyLed",                    
+    PinState.LOW
+  )
 
-  // schedule a task to toggle the state of the LED every 1 second
-  val blinkTask: Future[Unit] = Future {
-    while (true) {
-      myLed.toggle()
-      Thread.sleep(1000)
-    }
-  }
+  println(greeting)
+}
 
-  // stop the blinking after 10 seconds
-  val stopBlinkingTask: Future[Unit] = Future {
-    Thread.sleep(10000)
-    blinkTask.foreach(_ => println("Stopping blinking..."))
-    gpio.shutdown()
-  }
-
-  // Wait for the blinking to stop
-  stopBlinkingTask.onComplete(_ => println("Blinking stopped."))
-
-  // Keep the application running
-  while (true) {
-    Thread.sleep(1000)
-  }
-
+trait Greeting {
+  lazy val greeting: String = "hello"
 }
