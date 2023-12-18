0ae3aa426e406369e330ca9095c7f824b9b6511d
carbonnm <marie.carbonnelle@student.unamur.be>
2023-12-18 10:13:01 +0100

    branche clean

--- sensors/ambiance/smartRPG.py
+++
@@ -1,5 +0,0 @@
-import RPi.GPIO as GPIO
-import requests
-import threading
-import time
-
--- sensors/led.py
+++
@@ -1,7 +0,0 @@
-import board
-import neopixel
-pixels = neopixel.NeoPixel(board.D18, 60)
-
-#pixels[0] = (255, 0, 0)
-
-pixels.fill((0, 255, 0))
