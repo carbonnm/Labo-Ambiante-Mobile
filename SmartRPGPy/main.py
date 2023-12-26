a5f68b054b361ae703800fd247857e57b68d451c
carbonnm <marie.carbonnelle@student.unamur.be>
2023-12-26 15:10:40 +0100

    Update main.py

--- SmartRPGPy/main.py
+++ SmartRPGPy/main.py
@@ -5,12 +5,14 @@ from ServoMotorActor import ServoMotorActor
 from LedActor import LedActor
 
 #Lancement des acteurs
+"""
 servo_actor = ServoMotorActor.start(pin=11)
 rfid_actor = RFIDReaderActor.start(servo_actor)
-led_actor = LedActor.start()
 
 #Démarrer la lecture de tags RFID
 rfid_actor.tell({'command': 'start_reading'})
+"""
+led_actor = LedActor.start()
 
 led_actor.tell({'command': 'volcan'})
 
