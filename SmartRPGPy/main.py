11e544322283a873a05d45f62cb6a79b13fbd44d
carbonnm <marie.carbonnelle@student.unamur.be>
2023-12-26 14:24:02 +0100

    ajout du pin pour le servomoteur

--- SmartRPGPy/main.py
+++ SmartRPGPy/main.py
@@ -3,7 +3,7 @@ from RFIDReaderActor import RFIDReaderActor
 from ServoMotorActor import ServoMotorActor
 
 #Lancement des acteurs
-servo_actor = ServoMotorActor.start()
+servo_actor = ServoMotorActor.start(pin=11)
 rfid_actor = RFIDReaderActor.start(servo_actor)
 
 #Démarrer la lecture de tags RFID
