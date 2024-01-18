7a559dcbdf4662e297dad14f82a2e2f366679ce8
carbonnm <marie.carbonnelle@student.unamur.be>
2024-01-18 10:33:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,23 +4,16 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Install coursier
-curl -fL https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz | gzip -d > cs && chmod +x cs && ./cs setup
-
-# Add sbt repository
-echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
-echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-sudo apt-get update
-sudo apt-get install -y sbt
+# Install Mosquitto MQTT broker and clients
+sudo apt-get install -y mosquitto mosquitto-clients
 
 # Install Python packages
-sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel --break-system-packages
-sudo python3 -m pip install --force-reinstall rpi_ws281x adafruit-circuitpython-neopixel adafruit-blinka --break-system-packages
+sudo pip3 install -y rpi_ws281x adafruit-circuitpython-neopixel --break-system-packages
+sudo python3 -m pip install -y --force-reinstall rpi_ws281x adafruit-circuitpython-neopixel adafruit-blinka paho-mqtt keyboard --break-system-packages
 
 # Clone the Git repository
-mkdir -p ~/Projects
-cd ~/Projects
+mkdir -p ~/App
+cd ~/App
 git clone -b clean https://github.com/carbonnm/Labo-Ambiante-Mobile.git
 
 echo "Setup completed successfully!"
