08f7c984d08191ef4a0ec55e2ccaf0737e784633
carbonnm <marie.carbonnelle@student.unamur.be>
2024-01-18 20:35:58 +0100

    envoi des messages MQTT par le livre

--- BookSensor/book.py
+++ BookSensor/book.py
@@ -1,19 +1,26 @@
 import RPi.GPIO as GPIO
 import time
+from MQTT.mqtt_publisher import MQTTPublisher
 
 GPIO.setmode(GPIO.BOARD)
 
-pages_pin = [7, 11, 13, 15]
+pages_pin = {1 : 7, 2 : 11, 3 : 13, 4 : 15}
 baguette_pin = 6
+
+#Initialisation du publisher MQTT
+mqtt_publisher = MQTTPublisher("192.168.0.238", 1883, "SmartRPG", "SmartRPG", "channel_ambiances")
 
 for pin in pages_pin:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
     while True:
-        for pin_button in pages_pin:
-            if GPIO.input(pin_button) == GPIO.LOW:
+        for pin_button, broche in pages_pin.items():
+            if GPIO.input(broche) == GPIO.LOW:
                 print(f"Page numéro {pin_button} touchée")
+                #Envoi d'un message MQTT correspondant au numéro de page touché
+                mqtt_publisher.publish_message(pin_button, "channel_ambiances")
+
         time.sleep(0.1)
 
 except KeyboardInterrupt:
